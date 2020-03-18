d6b4d2601b3cb82acdbea04d3c20b1423753f822
alejandratub <alejandra.tubilla@gmail.com>
2020-03-17 21:40:01 -0600

    Cambios

--- Automatas.py
+++ Automatas.py
@@ -1,214 +1,224 @@
+#Alejandra Tubilla      A01022960
+#Joan Andoni 
+#German Torres          A01651423
+
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
 import pylab
 
-states = [0]
-last_state = 0
-counter_state = 0
+estados = [0]
+estadoFinal = 0
+estadoActual = 0
 automata = []
-alphabet = []
-special = ['(', ')', '|']
+alfabeto = []
+caracteresEspeciales = ['(', ')', '|']
 
-def createNFA(regex, star, finish): 
+#Funcion para crear el NFA a partir de la expresion regular
+def NFA(expresionRegular, star, fin): 
 
-    print("REGES", regex, star)
+    global estadoFinal
+    global estadoActual
+    padre = False
+    contador = 0
+    expresionRegularParte = ''
+    estadoAnterior = estadoFinal
 
-    global last_state
-    global counter_state
-    parent = False
-    p_counter = 0
-    mini_regex = ''
-    state_before = last_state
 
     if star == True:
 
-        state = [last_state, 'ep', last_state+1+counter_state]
-        last_state += (1 + counter_state)
-        if counter_state != 0: counter_state = 0
+        state = [estadoFinal, 'ep', estadoFinal+1+estadoActual]
+        estadoFinal += (1 + estadoActual)
+        if estadoActual != 0: estadoActual = 0
 
         if state not in automata:
             automata.append(state)
 
-    for index, char in enumerate(regex):
+    #Dividir la expresion regular por partes
+    for index, caracter in enumerate(expresionRegular):
 
-        if char not in alphabet and char not in special:
-            alphabet.append(char)
+        if caracter not in alfabeto and caracter not in caracteresEspeciales:
+            alfabeto.append(caracter)
+        #Dentro del parentesis
+        if caracter == '(':
+            padre = True            
+            if contador > 0:
+                expresionRegularParte+= caracter
 
-        if char == '(':
-            parent = True            
-            if p_counter > 0:
-                mini_regex+= char
+            contador += 1
 
-            p_counter += 1
+        elif caracter == ')':
 
-        elif char == ')':
+            contador -= 1
 
-            p_counter -= 1
+            if contador > 0:
+                expresionRegularParte+= caracter
 
-            if p_counter > 0:
-                mini_regex+= char
+            elif contador == 0:
+                if index < len(expresionRegular) -1 and expresionRegular[index +1] == '*':
 
-            elif p_counter == 0:
-                if index < len(regex) -1 and regex[index +1] == '*':
-
-                    createNFA(mini_regex, True, 0)
+                    NFA(expresionRegularParte, True, 0)
 
 
                 else: 
-                    createNFA(mini_regex, False, 0)
+                    NFA(expresionRegularParte, False, 0)
 
-                mini_regex = ''
+                expresionRegularParte = ''
 
-        elif p_counter > 0:
-                mini_regex+= char
-       
-    if parent == False:
-        if '|' in regex:
-            or_states = regex.split('|')
-            f_states = regex.replace('|', '')
-            Num_f_state = last_state + len(f_states) +1
+        elif contador > 0:
+                expresionRegularParte+= caracter
+    #con OR
+    if padre == False:
+        if '|' in expresionRegular:
+            estadoOR = expresionRegular.split('|')
+            estadosF = expresionRegular.replace('|', '')
+            numeroEstadosF = estadoFinal + len(estadosF) +1
 
-            or_las_state = last_state
+            estadoFinalOR = estadoFinal
 
-            for or_state in or_states:
+            for estado_OR in estadoOR:
 
-                for index, char in enumerate(or_state):
+                for index, caracter in enumerate(estado_OR):
                     
                     if index == 0:
-                        state = [or_las_state, char, last_state + 1 +counter_state]
+                        state = [estadoFinalOR, caracter, estadoFinal + 1 +estadoActual]
                     else:
-                        state = [last_state, char, last_state + 1 +counter_state]
+                        state = [estadoFinal, caracter, estadoFinal + 1 +estadoActual]
 
-                    last_state += (1 + counter_state)
-                    if counter_state != 0: counter_state = 0
+                    estadoFinal += (1 + estadoActual)
+                    if estadoActual != 0: estadoActual = 0
 
                     if state not in automata:
                         automata.append(state)
 
-                    if last_state not in states:
-                        states.append(last_state)
+                    if estadoFinal not in estados:
+                        estados.append(estadoFinal)
 
-                    if index == len(or_state) -1:       
-                        state = [last_state, 'ep', Num_f_state]
+                    if index == len(estado_OR) -1:       
+                        state = [estadoFinal, 'ep', numeroEstadosF]
                         if state not in automata:
                             automata.append(state)
 
             if star == True: 
 
-                state = [Num_f_state, 'ep', or_las_state]
+                state = [numeroEstadosF, 'ep', estadoFinalOR]
                 if state not in automata:
                     automata.append(state)
 
                 star = False
 
-            last_state = or_las_state            
+            estadoFinal = estadoFinalOR            
 
-            if last_state not in states:
-                states.append(last_state)
+            if estadoFinal not in estados:
+                estados.append(estadoFinal)
 
         else: 
 
             star_states = 1
 
-            for char in regex:
-                state = [last_state, char, last_state +1]
-                last_state += 1
+            for caracter in expresionRegular:
+                state = [estadoFinal, caracter, estadoFinal +1]
+                estadoFinal += 1
                 star_states += 1
 
                 if state not in automata:
                     automata.append(state)
 
             if star == True:
-                state = [last_state, 'ep', state_before]
+                state = [estadoFinal, 'ep', estadoAnterior]
                 if state not in automata:
                     automata.append(state)
 
-                counter_state = star_states
-                last_state = state_before
+                estadoActual = star_states
+                estadoFinal = estadoAnterior
 
     if star:
-        state = [last_state, 'ep', state_before]
-        last_state = state_before
+        state = [estadoFinal, 'ep', estadoAnterior]
+        estadoFinal = estadoAnterior
 
-def epsilon_closure(states):
+#Funcion del epsilon closure
+def epsilonClosure(estados):
     
-    for state in states:
-        for nfa_state in automata:
-            if nfa_state[0] == state and nfa_state[1] == 'ep' and not nfa_state[2] in states:
-                 states.append(nfa_state[2])
+    for state in estados:
+        for estadoNFA in automata:
+            if estadoNFA[0] == state and estadoNFA[1] == 'ep' and not estadoNFA[2] in estados:
+                 estados.append(estadoNFA[2])
     
-    return states
+    return estados
 
-def move(state,symbol):
-    temp = []
+#Funcion para los movimientos
+def move(state,simbolo):
+    temporal = []
     for s in state:
-        for nfa_state in automata:
-            if nfa_state[0] == s and nfa_state[1] == symbol and not nfa_state[2] in temp:
-                temp.append(nfa_state[2])
+        for estadoNFA in automata:
+            if estadoNFA[0] == s and estadoNFA[1] == simbolo and not estadoNFA[2] in temporal:
+                temporal.append(estadoNFA[2])
 
-    temp = epsilon_closure(temp)
+    temporal = epsilonClosure(temporal)
 
-    return temp
+    return temporal
 
-def isNew(state,new_states):
+#Funcion para estados nuevos
+def nuevo(state,estadosNuevos):
 
-    for n in new_states:
+    for n in estadosNuevos:
         if set(n) == set(state):
             return 0
 
     return 1
 
-def NFA_to_DFA(nfa_initial_state,nfa_final_state,alphabet):
+#Funcion para convertir de NFA a DFA
+def NFAtoDFA(inicialNFA,finalNFA,alfabeto):
 
     dfa = {}
     DFA = []
-    current_state = []
-    final_states = []
-    new_states = []
+    estadoActual = []
+    estadosFinales = []
+    estadosNuevos = []
 
 
-    initial_state = [nfa_initial_state]
+    estadoInicial = [inicialNFA]
 
-    current_state = epsilon_closure(initial_state)
+    estadoActual = epsilonClosure(estadoInicial)
     
-    new_states.append(current_state)
+    estadosNuevos.append(estadoActual)
 
-    if nfa_final_state in current_state:
-        final_states.append(current_state)
+    if finalNFA in estadoActual:
+        estadosFinales.append(estadoActual)
 
-    for state in new_states:
-        for symbol in alphabet:
+    for state in estadosNuevos:
+        for simbolo in alfabeto:
 
-            current_state = move(state,symbol)
+            estadoActual = move(state,simbolo)
 
-            if current_state:
+            if estadoActual:
 
-                if isNew(current_state,new_states):
-                    new_states.append(current_state)
-                    if nfa_final_state in current_state:
-                        final_states.append(current_state)
+                if nuevo(estadoActual,estadosNuevos):
+                    estadosNuevos.append(estadoActual)
+                    if finalNFA in estadoActual:
+                        estadosFinales.append(estadoActual)
 
-                DFA.append([new_states.index(state),symbol,new_states.index(current_state)])
+                DFA.append([estadosNuevos.index(state),simbolo,estadosNuevos.index(estadoActual)])
 
     dfa["dfa"] = DFA
-    dfa["states"] = new_states
-    dfa["final_states"] = final_states
+    dfa["estados"] = estadosNuevos
+    dfa["estadosFinales"] = estadosFinales
 
     return dfa
-    
-def EvaluateString(cadena, ODFA):
+
+#Funcion para evaluar los strings y ver si cumplen    
+def evaluarEntrada(cadena, ODFA):
     dfa = ODFA["dfa"]
     ini = 0
     valid = False
 
-    for char in cadena:
+    for caracter in cadena:
 
         found = False
 
         for index, state in enumerate(dfa):
             if state[0] == ini:
-                if state[1] == char:
+                if state[1] == caracter:
                     found = True
                     ini = state[2]
                     break
@@ -218,22 +228,25 @@ def EvaluateString(cadena, ODFA):
             ini = -1
 
                 
-    for final in ODFA["final_states"]:
-        if ini == ODFA["states"].index(final):
+    for final in ODFA["estadosFinales"]:
+        if ini == ODFA["estados"].index(final):
             valid = True
     
     return valid
 
 
-regex = input("Enter Regex\n")
-createNFA(regex, False, 1)
+#OUTPUT A USUARIO Y LLAMADAS A FUNCIONES
 
-dfa = NFA_to_DFA(0, last_state, alphabet)
+print("***************EXPRESIONES REGULARES A AUTOMATAS*****************")
+expresionRegular = input("Ingresa la expresión regular para la creacion de automatas:\n")
+NFA(expresionRegular, False, 1)
+
+dfa = NFAtoDFA(0, estadoFinal, alfabeto)
 
 # Grafico
 g = nx.DiGraph()
 g2 = nx.DiGraph()
-g.add_nodes_from(states)
+g.add_nodes_from(estados)
 
 # Datos de NFA
 print("NFA")
@@ -249,9 +262,9 @@ for a in dfa["dfa"]:
     g2.add_edge(a[0], a[2])
     print(a)
 
-print("DFA final states:")
-for final in dfa["final_states"]:
-    print(dfa["states"].index(final))
+print("Estados Finales DFA:")
+for final in dfa["estadosFinales"]:
+    print(dfa["estados"].index(final))
 
 
 nx.draw(g,with_labels=True, arrows=True)
@@ -262,6 +275,14 @@ nx.draw(g2,with_labels=True, arrows=True)
 plt.draw()
 plt.show()
 
+entradaRevision = input("¿Desea analizar un string?: (Si/No)\n")
+
 while True:
-    cadena = input("Enter string\n")
-    print(EvaluateString(cadena, dfa))
+    if entradaRevision == "Si":
+        cadena = input("Ingrese la entrada a analizar\n")
+        print(evaluarEntrada(cadena, dfa))
+        entradaRevision = input("¿Desea analizar un nuevo string?: (Si/No)\n")
+
+    else:
+        print("Programa terminado")
+        break
